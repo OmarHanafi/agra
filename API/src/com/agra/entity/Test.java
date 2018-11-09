13c2baab8e3e65753c552cf5a56cfdd45cedc50f
Omar Hanafi <omarhanafi789@gmail.com>
2018-11-09 16:13:30 +0000

    First commit

---
+++ API/src/com/agra/entity/Test.java
@@ -0,0 +1,25 @@
+package com.agra.entity;
+
+public class Test {
+	
+	public String name;
+	
+	public Test(){
+		
+	}
+
+	public Test(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
