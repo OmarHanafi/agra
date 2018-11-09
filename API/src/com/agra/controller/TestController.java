fc4be4cfb885c7a5e4c4931de160fb3dfc7be915
DESKTOP-CL0UM13\Adib_ <adib_houssam@hotmail.com>
2018-11-09 18:38:52 +0100

    tested its working

--- API/src/com/agra/controller/TestController.java
+++ API/src/com/agra/controller/TestController.java
@@ -12,28 +12,36 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.agra.entity.*;
+import com.agra.service.UserService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class TestController {
 	
 	@Autowired
-	SessionFactory sessionFactory;
+	private SessionFactory sessionFactory;
+	@Autowired
+	private UserService userService;
 	
-	@Transactional
+	
+	
 	@GetMapping("/home")
-	public String test(){
-		Session session = sessionFactory.getCurrentSession();
-		User user = session.get(User.class, "admin");
-		return user.toString();
+	public User test(@RequestParam("username") String username,Model theModel){
+		
+		System.out.println(username);
+		User user = userService.getUser(username);
+		return user;
 	}
 	
 }
--- API/src/com/agra/service/UserServiceImpl.java
+++ API/src/com/agra/service/UserServiceImpl.java
@@ -1,16 +1,23 @@
 package com.agra.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.agra.dao.UserDAO;
 import com.agra.entity.User;
 
 @Service
 public class UserServiceImpl implements UserService{
 
+	@Autowired
+	private UserDAO userDAO;
+	
 	@Override
-	public User getUser() {
-		// TODO Auto-generated method stub
-		return null;
+	@Transactional
+	public User getUser(String username) {
+		return this.userDAO.getUser(username);
 	}
 
 }
