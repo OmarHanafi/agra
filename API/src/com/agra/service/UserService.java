8150a840ae041f6cd65f95b788fe9416b5d34fac
Omar Hanafi <omarhanafi789@gmail.com>
2018-11-09 18:15:39 +0000

    Service User

--- API/src/com/agra/service/UserService.java
+++ API/src/com/agra/service/UserService.java
@@ -4,6 +4,6 @@ import com.agra.entity.User;
 
 public interface UserService {
 	
-	public User getUser();
+	public User getUser(String username);
 	
 }
--- API/src/com/agra/service/UserServiceImpl.java
+++ API/src/com/agra/service/UserServiceImpl.java
@@ -1,16 +1,20 @@
 package com.agra.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.agra.dao.UserDAO;
 import com.agra.entity.User;
 
 @Service
 public class UserServiceImpl implements UserService{
 
+	@Autowired
+	UserDAO userDAO;
+	
 	@Override
-	public User getUser() {
-		// TODO Auto-generated method stub
-		return null;
+	public User getUser(String username) {
+		return this.userDAO.getUser(username);
 	}
 
 }
