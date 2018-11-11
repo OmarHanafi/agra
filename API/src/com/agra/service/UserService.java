7aca1b2e9583e6e3d29425c9343c999dc78ad907
Omar Hanafi <omarhanafi789@gmail.com>
2018-11-11 19:05:16 +0000

    Service

--- API/src/com/agra/service/UserService.java
+++ API/src/com/agra/service/UserService.java
@@ -4,10 +4,6 @@ import com.agra.entity.User;
 
 public interface UserService {
 	
-<<<<<<< HEAD
 	public boolean login(User user);
-=======
-	public User getUser(String username);
->>>>>>> c96eaab32c059b2c3524c78ff11d36a38ad46cf6
 	
 }
