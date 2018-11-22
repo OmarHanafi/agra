8b87986065207b9b1bad388c351daaec7deb6a94
DESKTOP-CL0UM13\Adib_ <adib_houssam@hotmail.com>
2018-11-22 22:21:51 +0100

    no message

--- AngularProject/src/app/client/pages/clientcategory/clientcategory.component.ts
+++ AngularProject/src/app/client/pages/clientcategory/clientcategory.component.ts
@@ -28,23 +28,20 @@ export class ClientCategoryComponent implements OnInit {
               private paginationService : PaginationService) {
 
                 route.params.subscribe(val =>{
-
-                  console.log("param changed !!")
                   this.categoryId = parseInt(this.route.snapshot.paramMap.get('id'));    // Getting the product's id
-                  console.log(this.categoryId)
                   this.clientService.getCategoryProducts(this.categoryId)
                   .subscribe((data) => {
                     this.products = data;                                                // Getting the products
                     this.totalPages = Math.ceil(this.products.length/this.perPage);
                     this.setPage(this.currentPage);
                   });                         
-              
                   this.clientService.getCategory(this.categoryId)
                   .subscribe((data)=> this.category = data);                             // Getting the category
                 })
   }
 
   setPage(page : number){
+    this.currentProducts=null;
     if(page > 0 && page <= this.totalPages){
       this.currentPage = page;
       this.currentProducts = this.paginationService.getPage<Product>(this.products,page,this.perPage); // pour rendre le bon nombre de produit a partir d'un numero exact
